db152df4bab6f99f2e3bae19ba1eb91a82ec04ae
abmohamm <mohd.92java@gmail.com>
2020-11-22 20:49:43 -0500

    [US1234]:Created Spring Boot, MongoDB skeleton Project

---
+++ springboot-crud-restful-api-mongodb/src/main/java/com/app/samples/springboot/SpringbootCrudRestfulApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.app.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulApiMongodbApplication.class, args);
+	}
+
+}
